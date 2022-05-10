cf4ea1dcdabf7d47252c37ad7133727174e582c5
Allen <aallen@nrel.gov>
2022-05-10 15:07:54 -0600

    updates for GeoJSON pre-processing

--- GeoJSON_Processing/Py_GeoJSON_Test.py
+++ GeoJSON_Processing/Py_GeoJSON_Test.py
@@ -4,13 +4,15 @@ import pandas as pd
 
 #Set names of input files 
 input_data='test_data.csv'
-geojson_file='test_file.json'
+urbanopt_geojson_file='uo_test_file.json'
+input_geojson_file='input_test_file.json'
 
 #get list of IDs in the file 
 
 #apply the below chunk to all IDs in the CSV 
 
 data=pd.read_csv(input_data) 
+
 
 
 def read_data(geojson_file): 
@@ -29,7 +31,10 @@ def process_item(ident, sys_type, geojson_file): #make that the list of IDs from
     json.dump(geojson, f)
     f.close()
 
-results = [process_item(row[0], row[1], geojson_file) for row in zip(data['id'], data['system_type'])] 
+geojson = read_data(input_geojson_file)
+results = [process_item(row[0], row[1], urbanopt_geojson_file) for row in zip(data['id'], data['system_type'])] 
+output=[bldg['properties']['PARCELKEY'] for bldg in geojson['features']]
+df = pd.DataFrame(output)
 
 
 #loop thru the geojson data and the input data and update the geojson properties for each key in the input data file 
